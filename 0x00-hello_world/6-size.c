cb40e80792d505ee0ab84daee3f2d1ec04b6be99
juiceosam <mbarie.emmanuel05@gmail.com>
2023-09-11 18:13:12 -0700

    alx

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry Point for the program
+ *
+ * Return: 0 if success, non zero if error
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
